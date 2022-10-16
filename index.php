10281c583923a2836f1e24c295bcc2e9b53a513a
Andreas Chatel <andreas.chatel5@gmail.com>
2022-10-16 12:56:10 +0200

    petits tests

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
-echo "bjour tout le monde";
+/*A FAIRE :
+    fonction pour :
+        supprimer une annonce
+        modifier une annonce
+        avoir une liste de toute les annonces
+        mettre une annonce en favoris ?
+
+REMARQUE :        
+module sert seulement a comuniquer avec la bdd
+fitre par critères dans le controller 
+
+je sais pas comment on relie les different pages a des fonctions dans le controller...
+*/
 
 // sera dans "module", les methode seront utilisé par "controller" et envoyé vers "vue"
-
-class annonce {
+class annonce 
+{
     // Properties
-    private $titre;
-    private $prix;
-    private $description;
-    private $categories;
-    private $id;
+    public $titre;
+    public $prix;
+    public $description;
+    public $categories;
+    private $id; /* peut etre pas une bonne idée de mettre l'id en public, idk */
   
     // Methods
+    /*permet d'instancier une nouvelle annonce, peut etre mettre du SQL plus tard pour mettre dans la bdd ?*/
     function set_annonce($titre, $prix, $description, $categories, $id) {
       $this->titre = $titre;
       $this->prix = $prix;
@@ -20,28 +33,21 @@ class annonce {
       $this->id = $id;
 
     }
+    /*donne sous forme de liste tout les elements d'une annonce*/
     function get_annonce() {
-      return $this->titre;
-      return $this->prix;
-      return $this->description;
-      return $this->categories;
-      return $this->id;
+      return array($this->titre, $this->prix, $this->description, $this->categories, $this->id);
     }
 
-    '''
-    A FAIRE :
-        fonction pour :
-            supprimer une annonce
-            modifier une annonce
-            avoir une liste de toute les annonces
-            mettre une annonce en favoris ?
-
-    REMARQUE :        
-    module sert seulement a comuniquer avec la bdd
-    fitre par critères dans le controller 
     
-    je sais pas comment on relie les different pages a des fonctions dans le controller...
 
-
-    '''
   }
+
+  $annonce1 = new annonce();
+  $annonce1->set_annonce("first_titre","first","first","first","first_id",);
+
+  var_dump($annonce1->get_annonce());
+  echo "<br>";
+  echo $annonce1->titre;
+ 
+
+  ?>
