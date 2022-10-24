e8563984025e0211b951de91fdb55df9971de2d9
Antoine <antoinewellers@hotmail.fr>
2022-10-24 20:56:56 +0200

    Commit de la vie
    -affiche ou cache le bouton réponse ainsi que le formulaire de réponse si c'est l'utilisateur connecté qui l'a écrit
    -popup de confirmation d'envoi de message

--- Messagerie/insert_message.php
+++ Messagerie/insert_message.php
@@ -1,9 +1,9 @@
 <?php
     include("connexion.php");
     
-    $requete= "INSERT INTO `message` VALUES (NULL, '$montexte', '$id_receveur', '$id_envoi', '$id_annonce')"; 
+    $requete= "INSERT INTO `message` VALUES (NULL, '$montexte', '$id_receveur', '$id_envoi', '$id_annonce', '$id_connect')"; 
     $message_db = $db->prepare($requete);
     $message_db->execute();
-    echo "message intégré, vas voir!"
+    echo '<script>alert("Message bien envoyé!!")</script>'; //popup pour confirmer l'envoi du message
 ?>
 
